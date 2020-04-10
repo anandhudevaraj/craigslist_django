977e7481879cc457ca280924a44541f8d4e487ed
anandhu <anandhudevaraj@gmail.com>
2020-04-10 19:48:04 +0530

    Added search field

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 
+app_name = 'my_app'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('new_search/', views.new_search, name='new_search')
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+
+def new_search(request):
+    search_item = request.POST.get('search')
+    print(search_item)
+    return render(request, 'my_app/new_search.html')
